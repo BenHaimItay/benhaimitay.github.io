8f8b22f67b24597edddb44145f252b9294919952
Itay Ben Haim <benhaimitay@gmail.com>
2024-05-10 12:18:09 +0300

    add gh-actions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-const path = require('path')
- 
+const path = require("path");
+const nextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
+
 module.exports = {
+  output: "export",
+  reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+  },
+};
